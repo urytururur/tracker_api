f9f811c0a180f087e3aa9a2aaf91cabd1f24e7cc
Torbjörn Ekstedt <Torbjorn.Ekstedt@hotmail.com>
2022-03-19 19:30:40 +0100

    fixat authorization med jwt

---
+++ custom_modules/time_periods_in_ms.js
@@ -0,0 +1,15 @@
+const SECOND = 1000;
+const MINUTE = SECOND * 60;
+const HOUR = MINUTE * 60;
+const DAY = HOUR * 24
+const WEEK = DAY * 7
+const YEAR = WEEK * 52;
+
+module.exports = {
+    SECOND: SECOND,
+    MINUTE: MINUTE,
+    HOUR: HOUR,
+    DAY: DAY,
+    WEEK: WEEK,
+    YEAR: YEAR
+}
--- server/authServers/userAuthServer.js
+++ server/authServers/userAuthServer.js
@@ -1,11 +1,78 @@
+const dotenv = require('dotenv');
 const express = require('express')
 const dbFacade = require('../db/dbFacade.js')
+const TIME_PERIODS = require('../../custom_modules/time_periods_in_ms.js')
+var jwt = require('jsonwebtoken');
+const cookieParser = require("cookie-parser");
 
 const app = express()
 const port = 5000
 
+//middleware functions
 app.use(express.json())
+app.use(cookieParser());
 
+function authenticateToken(req, res, next) {
+
+  if (req.cookies.token == null)
+  {
+    res.status(401)
+    return res.json({
+      message: "You are not signed in."
+    })
+  }
+
+  jwt.verify(req.cookies.token, process.env.SHA256_TOKEN_KEY, (err, user) => {
+    if (err)
+    {
+      console.log(err)
+
+      res.status(403)
+      return res.json({
+        message: "Forbidden request."
+      }) 
+    }
+    req.user = user
+    next()
+  })
+}
+
+let refreshTokens = [];
+
+app.post('/refreshToken', authenticateToken, (req, res) => {
+
+  if (!req.cookies.refreshtoken) {
+    return res.sendStatus(401);
+  }
+
+  if (!refreshTokens.includes(req.cookies.refreshtoken)) {
+      return res.sendStatus(403);
+  }
+
+  jwt.verify(req.cookies.refreshtoken, process.env.SHA256_REFRESHTOKEN_KEY, (err, user) => {
+      if (err) {
+          return res.sendStatus(403);
+      }
+
+      jwt.sign({email: user.email}, process.env.SHA256_TOKEN_KEY, {
+        algorithm: 'HS256',
+        expiresIn: '20m'
+      }, (err, token) => {
+
+        res.cookie("token", token, {
+          //secure: process.env.NODE_ENV !== "development",
+          httpOnly: true,
+          maxAge:  20 * TIME_PERIODS.MINUTE
+        });
+
+        res.json({
+          message: "Successfully refreshed token."
+        });
+      });
+  });
+});
+
+//routes
 app.post('/signup', (req, res) => {
   dbFacade.userExists(req.body.email)
     .then((response) => {
@@ -31,25 +98,66 @@ app.post('/signup', (req, res) => {
 app.post('/login', (req, res) => {
   //TODO:
   //validatea email och password mot databasen
-    //om ej valid
-      //rejecta requesten med ett meddelande "Wrong username or password"
-    //om valid -> generera
-      //generera jwt
-      //sätt jwt:n som en säker cookie hos clienten (!read, !write)
+  dbFacade.validUserCredentials(req.body.email, req.body.password)
+  .then((response) => {
+    if(!response.returnValue)
+    {
+      res.status(401)
+      res.json({message: "Wrong email or password."})
+    }
+    else
+    {
+      const userData = { //to be stored in jwt token
+        email: req.body.email,
+      };
+
+      jwt.sign(userData, process.env.SHA256_TOKEN_KEY, {
+        algorithm: 'HS256',
+        expiresIn: '20m'
+      }, (err, token) => {
+
+        res.cookie("token", token, {
+          //secure: process.env.NODE_ENV !== "development",
+          httpOnly: true,
+          maxAge:  20 * TIME_PERIODS.MINUTE
+        });
+
+        jwt.sign(userData, process.env.SHA256_REFRESHTOKEN_KEY, {
+          algorithm: 'HS256',
+        }, (err, refreshtoken) => {
+
+          refreshTokens.push(refreshtoken);
+
+          res.cookie("refreshtoken", refreshtoken, {
+            //secure: process.env.NODE_ENV !== "development",
+            httpOnly: true
+          });
+
+          res.status(200)
+          res.json({message: "Logged in successfully."})
+        });
+      });
+    }
+  })
 })
 
-app.post('/logout', (req, res) => {
+app.post('/logout', authenticateToken, (req, res) => {
+  console.log(req.user);
   //TODO:
   //ta bort den säkra cookien med jwt:n från clienten
+  //avregistrera jwt:n
+  res.clearCookie('token');
+  res.clearCookie('refreshtoken');
+  res.send("Successfully signed out.")
 })
 
-app.post('/deleteAccount', (req, res) => {
+app.post('/deleteAccount', authenticateToken, (req, res) => {
 
-  dbFacade.validUserCredentials(req.body.email, req.body.password)
+  dbFacade.validUserCredentials(req.user.email, req.body.password)
     .then((response) => {
       if(response.returnValue)
       {
-        dbFacade.deleteAccount(req.body.email)
+        dbFacade.deleteAccount(req.user.email)
           .then((response) => {
             if(response.success)
             {
@@ -66,11 +174,11 @@ app.post('/deleteAccount', (req, res) => {
     })
 })
 
-app.post('/createToggleActivationRequest', (req, res) => {
+app.post('/createToggleActivationRequest', authenticateToken, (req, res) => {
   dbFacade.validToggleActivationRequest(
     req.body.serialNumber,
     req.body.hashedPhysicalSecurityKey,
-    req.body.email)
+    req.user.email)
       .then((response) => {
         if(response.returnValue)
         {
@@ -80,7 +188,7 @@ app.post('/createToggleActivationRequest', (req, res) => {
               {
                 dbFacade.createToggleActivationRequest(
                   req.body.serialNumber,
-                  req.body.email
+                  req.user.email
                 )
                 .then((response) => {
                   //ta bort requesten efter 10 sec
@@ -107,8 +215,7 @@ app.post('/createToggleActivationRequest', (req, res) => {
       })
 })
 
-app.post('/toggleTrackerActive', (req, res) => {
-  //check valid, loose security key
+app.post('/toggleTrackerActiveButton', (req, res) => {
   dbFacade.validTrackerCredentials(
     req.body.serialNumber,
     req.body.hashedPhysicalSecurityKey)
@@ -137,6 +244,7 @@ app.post('/toggleTrackerActive', (req, res) => {
 
 //initialization functions
 dbFacade.connect().then((response) => {
+  dotenv.config()
   app.listen(port, () => {
     console.log(`Auth server listening on port ${port}`)
   })
